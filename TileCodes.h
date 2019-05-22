0ac8702a0ae7b71da6ad14b5ffa472e82abb25b2
Khoa Vu <s3678490@csitprdap03.int.its.rmit.edu.au>
2019-05-22 20:35:54 +1000

    Submission Version

--- TileCodes.h
+++ TileCodes.h
@@ -18,20 +18,20 @@
 #define CLOVER 6
 
 //Set colours
-//#define SET_RED "\e[1;100;31m"
-//#define SET_ORANGE "\e[1;100;38;5;208m"
-//#define SET_YELLOW "\e[1;100;33m"
-//#define SET_GREEN "\e[1;100;32m"
-//#define SET_BLUE "\e[1;100;34m"
-//#define SET_PURPLE "\e[1;100;35m"
-//#define RESET_COLOUR "\e[0m"
+#define SET_RED "\e[1;100;31m"
+#define SET_ORANGE "\e[1;100;38;5;208m"
+#define SET_YELLOW "\e[1;100;33m"
+#define SET_GREEN "\e[1;100;32m"
+#define SET_BLUE "\e[1;100;34m"
+#define SET_PURPLE "\e[1;100;35m"
+#define RESET_COLOUR "\e[0m"
 
 //Set Shapes
-//#define SET_CIRCLE "ðŸ”´"
-//#define SET_STAR_4 "ðŸ”¥"
-//#define SET_DIAMOND "ðŸ””"
-//#define SET_SQUARE "â¬›"
-//#define SET_STAR_6 "ðŸ’¥"
-//#define SET_CLOVER "ðŸ‘�"
+#define SET_CIRCLE "ðŸ”´"
+#define SET_STAR_4 "ðŸ”¥"
+#define SET_DIAMOND "ðŸ””"
+#define SET_SQUARE "â¬›"
+#define SET_STAR_6 "ðŸ’¥"
+#define SET_CLOVER "ðŸ‘�"
 
 #endif // ASSIGN1_TILECODES_H
--- qwirkle.cpp
+++ qwirkle.cpp
@@ -8,13 +8,25 @@
 #define EXIT_SUCCESS    0
 
 /*
- NOTE: 
-
- 1) To get a shuffled tile bag every time playing game, please go into playGame() in GameEngine and uncomment shuffleAndCreateTileBag()
+  Deliverables:
+  Milestone 1:
+    15 Test cases: .input and .output in Test folder.
+  Milestone 2:
+    Fully functional and error-free (Memory leak error) implementation of basic gameplay.    Fix tile bag by default
+  Milestone 3:
+   Minor enhancements
+    1. Help! in menu section                                                                 Enable by default
+    2. Colour tiles when displayed on board                                                  Disable by default
+    3. Unicode/Emoji Tile when displayed on board                                            Disable by default
+    4. Expandable Board                                                                      Enable by default
+    5. High Scores keep record of all time highest score                                     Enable by default
+   Major enhancements
+    1. Place Multiples Tiles                                                                 Enable by default
+ 
+   NOTE: 1. To get a shuffled tile bag every time playing game, please go into playGame() in GameEngine and uncomment shuffleAndCreateTileBag()
  then comment out method tileBagForUnitTest(). To conduct unit test, just leave the program at its current state.
-
- 2) To get colour and Emoji code, please go into ... 
-
+         2. To enable Colour tiles when displayed on board: uncomment codes from line 55-72 in Tile.cpp
+         3. To enable Unicode/Emoji Tile when displayed on board: uncomment codes from line 74-102 in Tile.cpp 
 
  */
 int main(void) {
